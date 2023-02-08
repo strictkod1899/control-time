b1dfaf3005156d839916e13b10ba2634abc843c6
Konstantin <strictkod1899@yandex.ru>
2023-02-08 22:26:55 +0700

    feature: реализовать базовую структуру проекта

---
+++ deploy/deploy_local.ps1
@@ -0,0 +1,14 @@
+# Сборка проекта (обновление версии и push в ветку git не выполняется)
+# Скрипт запускать из корневой директории проекта
+
+param(
+	# optional
+	# Режим: prod, dev, feature
+	[string] $mode
+)
+
+if ($mode -eq $null -Or $mode -eq '') {
+    $mode = &"./deploy/other/get_deploy_mode_by_git_branch.ps1" | Select-Object -Last 1
+}
+
+./deploy/deploy.ps1 -mode "${mode}" -skipGit $True -skipUpdateVersion $True
